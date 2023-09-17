619b2037619e2846205896adcf978b6d86822e32
Tatyana Maltseva <t.malcevaa14@gmail.com>
2023-09-17 19:46:24 +0300

    feat: создать разметку

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,16 @@
 import React from "react"
 
 function Login() {
-  
+  return(
+    <section className="login">
+      <h2 className="login__title">Вход</h2>
+      <form className="login__form">
+        <input type="text" className="login__input login__input_type_email" name="email" placeholder="Email" />
+        <input type="text" className="login__input login__input_type_password" name="password" placeholder="Пароль" />
+        <button type="submit" className="login__button"></button>
+      </form>
+    </section>
+  )
 }
 
 export default Login;
