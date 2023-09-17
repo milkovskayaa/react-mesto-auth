b02229ae77684f29d22122914f426c03055800ea
Tatyana Maltseva <t.malcevaa14@gmail.com>
2023-09-17 20:52:27 +0300

    feat: отредактировать свойства инпутов

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,12 +10,14 @@ function Login() {
           className="login__input login__input_type_email"
           name="email"
           placeholder="Email"
+          required
         />
         <input
           type="text"
           className="login__input login__input_type_password"
           name="password"
           placeholder="Пароль"
+          required
         />
         <button type="submit" className="login__button">Войти</button>
       </form>
