584542c6027c108dec394c0596b7707c6a1fff25
Tatyana Maltseva <nucha1405@yandex.ru>
2023-09-18 21:48:22 +0300

    hot-fixes: отредактировать отступы

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
-  return(
+  return (
     <section className="login">
       <h2 className="login__title">Регистрация</h2>
       <form className="login__form">
@@ -20,11 +20,18 @@ function Register() {
           placeholder="Пароль"
           required
         />
-        <button type="submit" className="login__button">Зарегистрироваться</button>
+        <button type="submit" className="login__button">
+          Зарегистрироваться
+        </button>
       </form>
-      <span className="login__subtitle">Уже зарегистрированы? <Link to="/sign-in" className='login__link'>Войти</Link></span>
+      <span className="login__subtitle">
+        Уже зарегистрированы?{" "}
+        <Link to="/sign-in" className="login__link">
+          Войти
+        </Link>
+      </span>
     </section>
-  )
+  );
 }
 
 export default Register;
