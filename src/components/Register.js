210076f808b9fc8f3ad660515d3ff3bf0b52460d
Tatyana Maltseva <nucha1405@yandex.ru>
2023-09-18 21:20:58 +0300

    feat: создать разметку

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,27 @@ import React from "react";
 function Register() {
   return(
     <section className="register">
-
+      <h2 className="register__title">Вход</h2>
+      <form className="register__form">
+        <input
+          type="text"
+          className="register__input register__input_type_email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          className="register__input register__input_type_password"
+          name="password"
+          placeholder="Пароль"
+          required
+        />
+        <button type="submit" className="register__button">Зарегистрироваться</button>
+        <p className="register__link-to-login">
+          <a href="#" className="link" /> Уже зарегистрированы? Войти
+        </p>
+      </form>
     </section>
   )
 }
