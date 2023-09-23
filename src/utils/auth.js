663a9fd93b84c8678731c75cd5341c45d054a347
Tatyana Maltseva <nucha1405@yandex.ru>
2023-09-23 18:46:19 +0300

    feat: написать запросы для авторизации

---
+++ src/utils/auth.js
@@ -0,0 +1,54 @@
+export const BASE_URL = "https://auth.nomoreparties.co/";
+
+// запрос для регистрации
+export const register = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, password})
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((res) => {
+    return res;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+// запрос для авторизации
+export const authorize = (identifier, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({identifier, password})
+  })
+  .then((response => response.json()))
+  .then((data) => {
+    if (data.user){
+      localStorage.setItem('token', data.token);
+      return data;
+    }
+  })
+  .catch(err => console.log(err))
+}
+
+// запрос для проверки валидности токена
+export const getToken = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Athorization': `Bearer ${token}`
+        }
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+  
