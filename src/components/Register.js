4a56cf99a53cf5d9b7cc878b9046ca9fafaae449
Tatyana Maltseva <nucha1405@yandex.ru>
2023-09-23 19:11:35 +0300

    feat: сделать регистрацию

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,40 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import * as Auth from '../utils/Auth.js';
 
 function Register() {
+
+  const [formValue, setFormValue] = React.useState({
+    email: '',
+    password: ''
+  })
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setFormValue({
+      ...formValue,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Auth.register(formValue.email, formValue.password)
+    .then((res) => {
+      navigate('/sign-in', {replace: true})
+    })
+  }
+
   return (
     <section className="login">
       <h2 className="login__title">Регистрация</h2>
-      <form className="login__form">
+      <form className="login__form" onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
+          value={formValue.email}
           type="text"
           className="login__input login__input_type_email"
           name="email"
@@ -14,6 +42,8 @@ function Register() {
           required
         />
         <input
+          onChange={handleChange}
+          value={formValue.password}
           type="password"
           className="login__input login__input_type_password"
           name="password"
