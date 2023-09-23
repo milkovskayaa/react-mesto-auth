8a4d655ecdf8a5d143da9f0418480e2425c6283b
Tatyana Maltseva <nucha1405@yandex.ru>
2023-09-23 20:02:57 +0300

    feat: реализовать навигацию

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,35 @@
 import React from "react";
 import headerLogo from "../images/logo.svg";
-import {useLocation} from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
-function Header({loggedIn}) {
+function Header({ loggedIn }) {
   const location = useLocation();
 
   return (
-      <header className="header">
-        <img src={headerLogo} alt="Логотип" className="header__logo" />
-        <ul className="header__navigation">
+    <header className="header">
+      <img src={headerLogo} alt="Логотип" className="header__logo" />
+      <ul className="header__navigation">
         {/* {loggedIn ? <li className="header__link"></li> : ''} */}
-        {location.pathname === '/sign-up' ? <li className="header__link">Войти</li> : ''}
-        {location.pathname === '/sign-in' ? <li className="header__link">Регистрация</li> : ''}
-        </ul>
-      </header>
+        {location.pathname === "/sign-up" ? (
+          <li>
+            <Link to="sign-in" className="header__link">
+              Войти
+            </Link>
+          </li>
+        ) : (
+          ""
+        )}
+        {location.pathname === "/sign-in" ? (
+          <li>
+            <Link to="sign-in" className="header__link">
+              Регистрация
+            </Link>
+          </li>
+        ) : (
+          ""
+        )}
+      </ul>
+    </header>
   );
 }
 
